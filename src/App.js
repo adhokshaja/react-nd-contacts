2c040b6fb695b499915c2c516d7eaaf520dac24b
adhokshaja <adok.achar@gmail.com>
2017-07-01 20:45:27 -0400

    Initial setup for Contacts App from udactiy checklist

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class IntroPara extends Component{
-  render() {
-    return (
-      <p className="App-intro">
-        I've created this paragraph as a seperate component.
-      </p>
-    );
-  }
-}
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Testing</h2>
-        </div>
-        <IntroPara/>
+      <div>
+        Hello World!
       </div>
     );
   }
